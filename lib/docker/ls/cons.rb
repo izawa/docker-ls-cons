d0ca14cb340237c2ee55952e4563399596631ccc
いざわゆきみつ (Yukimitsu Izawa) <izawa@izawa.org>
2014-07-10 16:36:05 +0900

    initial version

---
+++ lib/docker/ls/cons.rb
@@ -0,0 +1,9 @@
+require "docker/ls/cons/version"
+
+module Docker
+  module Ls
+    module Cons
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/docker/ls/cons/version.rb
@@ -0,0 +1,7 @@
+module Docker
+  module Ls
+    module Cons
+      VERSION = "0.0.1"
+    end
+  end
+end
